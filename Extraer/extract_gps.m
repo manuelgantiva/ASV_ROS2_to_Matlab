ef82703f826b11c73df94034d8f8d2330f4489d3
Manuel Eduardo Gantiva Osorio <megantiva@uloyola.es>
2024-10-15 19:01:12 +0200

    Agregar topicos de los zonotopos

---
+++ Extraer/extract_gps.m
@@ -0,0 +1,32 @@
+function out = extract_gps(folder_name, topic_name)
+    % Selección del tópico
+    folderPath = fullfile(pwd,folder_name);
+    bag = ros2bagreader(folderPath);
+    bagSel = select(bag, "Topic", topic_name);
+    data = readMessages(bagSel);
+    % Inicialización de vectores
+    num_messages = length(data);
+    lat = zeros(num_messages, 1);
+    lon = zeros(num_messages, 1);
+    alt = zeros(num_messages, 1);
+    % time = zeros(num_messages, 1);
+    
+    % Extracción de datos
+    for i = 1:num_messages
+        % gps
+        lat(i, 1) = data{i}.latitude;
+        lon(i, 1) = data{i}.longitude;
+        alt(i, 1) = data{i}.altitude;
+        % tiempo
+        % format long
+        % sec = data{i, 1}.header.stamp.sec;
+        % nano = data{i, 1}.header.stamp.nanosec;
+        % nano = single(nano)/1e9;
+        % sec = single(mod(sec,1e6));
+        % time(i,1) = sec + nano;
+    end
+    time = double(bagSel.MessageList{:,1});
+    % time = time/1e9;
+    % Creación del vector de posición
+    out = [lat, lon, alt, time];
+end
---
+++ Figuras/figura_vel_dis.m
@@ -0,0 +1,100 @@
+function figura_vel_dis(angvel_data, linvel_data, obs_guille, obs_liu, directorio_destino)
+fig = figure('Name', 'Velocities and disturbances', 'NumberTitle', 'off');
+subplot(3,2,5);
+plot(angvel_data.tiempo,angvel_data.r_imu, 'b', 'LineWidth', 1);
+hold on
+plot(obs_guille.tiempo,obs_guille.r_hat, 'r', 'LineWidth', 1);
+plot(obs_liu.tiempo,obs_liu.r_hat, 'm', 'LineWidth', 1);
+xlim([0 inf]);
+grid on
+hold off
+xlabel('Time $(s)$','Interpreter','latex','FontSize',12);
+ylabel('Yaw $(rad~s^{-1})$','Interpreter','latex','FontSize',12);
+leg1 = legend('Measured $r$','Bejarano et al.','Liu el al.');
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',10);
+title('Estimation of $r$','Interpreter','latex','FontSize',15)
+
+%%
+subplot(3,2,1);
+plot(linvel_data.tiempo,linvel_data.u_imu, 'b', 'LineWidth', 0.5);
+hold on
+plot(obs_guille.tiempo,obs_guille.u_hat, 'r', 'LineWidth', 0.5);
+plot(obs_liu.tiempo,obs_liu.u_hat, 'm', 'LineWidth', 0.5);
+xlim([0 inf]);
+grid on
+hold off
+xlabel('Time $(s)$','Interpreter','latex','FontSize',12);
+ylabel('Surge $(m~s^{-1})$','Interpreter','latex','FontSize',12);
+leg1 = legend('Measured $u$','Bejarano et al.','Liu el al.');
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',10);
+title('Estimation of $u$','Interpreter','latex','FontSize',15)
+%%
+subplot(3,2,3);
+plot(linvel_data.tiempo,linvel_data.v_imu, 'b', 'LineWidth', 0.5);
+hold on
+plot(obs_guille.tiempo,obs_guille.v_hat, 'r', 'LineWidth', 0.5);
+plot(obs_liu.tiempo,obs_liu.v_hat, 'm', 'LineWidth', 0.5);
+xlim([0 inf]);
+grid on
+hold off
+xlabel('Time $(s)$','Interpreter','latex','FontSize',12);
+ylabel('Sway $(m~s^{-1})$','Interpreter','latex','FontSize',12);
+leg1 = legend('Measured $v$','Bejarano et al.','Liu el al.');
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',10);
+title('Estimation of $v$','Interpreter','latex','FontSize',15)
+%% 
+subplot(3,2,2);
+grid on
+plot(obs_guille.tiempo,obs_guille.su_hat, 'r', 'LineWidth', 0.5);
+hold on
+plot(obs_liu.tiempo,obs_liu.su_hat, 'm', 'LineWidth', 0.5);
+xlim([0 inf]);
+grid on
+hold off
+xlabel('Time $(s)$','Interpreter','latex','FontSize',12);
+ylabel('$\sigma_u~(m~s^{-2})$','Interpreter','latex','FontSize',12);
+leg1 = legend('Bejarano et al.','Liu el al.');
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',10);
+title('Estimation of $\sigma_u$','Interpreter','latex','FontSize',15)
+%% 
+subplot(3,2,4);
+grid on
+plot(obs_guille.tiempo,obs_guille.sv_hat, 'r', 'LineWidth', 0.5);
+hold on
+plot(obs_liu.tiempo,obs_liu.sv_hat, 'm', 'LineWidth', 0.5);
+xlim([0 inf]);
+grid on
+hold off
+xlabel('Time $(s)$','Interpreter','latex','FontSize',12);
+ylabel('$\sigma_v~(m~s^{-2})$','Interpreter','latex','FontSize',12);
+leg1 = legend('Bejarano et al.','Liu el al.');
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',10);
+title('Estimation of $\sigma_v$','Interpreter','latex','FontSize',15)
+%% 
+subplot(3,2,6);
+grid on
+plot(obs_guille.tiempo,obs_guille.sr_hat, 'r', 'LineWidth', 0.5);
+hold on
+plot(obs_liu.tiempo,obs_liu.sr_hat, 'm', 'LineWidth', 0.5);
+xlim([0 inf]);
+grid on
+hold off
+xlabel('Time $(s)$','Interpreter','latex','FontSize',12);
+ylabel('$\sigma_r~(rad~s^{-2})$','Interpreter','latex','FontSize',12);
+leg1 = legend('Bejarano et al.','Liu el al.');
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',10);
+title('Estimation of $\sigma_r$','Interpreter','latex','FontSize',15)
+
+
+% Obtener los manejadores de los ejes de todos los subplots
+ax = findall(gcf, 'Type', 'axes');
+% Enlazar los ejes x de todos los subplots
+linkaxes(ax, 'x');
+savefig(fullfile(directorio_destino, 'Vel_dist.fig'));
+end
